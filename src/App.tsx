3e0a6a6d99fd7126547360604d97ff46be5eec86
Masa11111 <m1s0h3d1@icloud.com>
2025-04-29 18:22:15 +0900

    AppLayoutを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import AppLayout from './components/layouts/AppLayout';
+import Home from './pages/Home';
+import NoMatchPath from './pages/NoMatchPath';
+import Report from './pages/Report';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/report' element={<Report />} />
+          <Route path='/' element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path='/report' element={<Report />} />
+            <Route path='*' element={<NoMatchPath />} />
+          </Route>
         </Routes>
       </Router>
     </>
---
+++ src/pages/NoMatchPath.tsx
@@ -0,0 +1,15 @@
+
+const NoMatchPath = () => {
+    return (
+        <div>
+            <div>
+                ページが存在しません。
+            </div>
+            <div>
+                正しいURLをご入力ください。
+            </div>
+        </div>
+    )
+}
+
+export default NoMatchPath
