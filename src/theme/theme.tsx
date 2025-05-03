938b7c4d336cc8326b03a7ad85fbb66eb888af22
Masa11111 <m1s0h3d1@icloud.com>
2025-05-03 22:12:02 +0900

    全体のフォントを設定

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -2,6 +2,9 @@ import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
   typography: {
-    fontFamily: 'Noto Sans JP, Roboto, "Helbetica Neue", Arial, sans-srif'
+    fontFamily: 'Noto Sans JP, Roboto, "Helbetica Neue", Arial, sans-srif',
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700
   }
 })
